282a5afbe8c90b9252b92a87c9cbab10af103715
mjamroz <jamroz@chem.uw.edu.pl>
2023-01-24 09:25:31 +0100

    fix ? heroku setup

--- setup.py
+++ setup.py
@@ -1,35 +1,13 @@
-import os
-from setuptools import setup, find_packages
-
-
-# Load version number from Thrift-Compiler-generated .py file
-constants_path = os.path.join(os.path.dirname(__file__), "lib", "evernote",
-                              "edam", "userstore", "constants.py")
-
-with open(constants_path) as constants_file:
-    constants = constants_file.read().split("\n")
-    for x in [x for x in constants if x.startswith('EDAM_VERSION')]:
-        exec(x)
-
-
-def read_from_same_directory(filename):
-    with open(os.path.join(os.path.dirname(__file__), filename)) as open_file:
-        content = open_file.read()
-    return content
-
+""" Setup """
+from setuptools import setup
 
 setup(
-    name='evernote3',
-    version="{major}.{minor}.0".format(major=EDAM_VERSION_MAJOR,
-                                       minor=EDAM_VERSION_MINOR),
-    author='Evernote Corporation',
-    author_email='api@evernote.com',
-    url='http://dev.evernote.com',
-    description='Evernote SDK for Python3',
-    license='BSD',
-    install_requires=[
-        'oauth2',
-        'oauthlib',
-        'requests_oauthlib'
-    ],
+    name="evernote3",
+    version="0.1",
+    author="Evernote Corporation",
+    author_email="api@evernote.com",
+    url="http://dev.evernote.com",
+    description="Evernote SDK for Python3",
+    license="BSD",
+    install_requires=["oauth2", "oauthlib", "requests_oauthlib"],
 )
